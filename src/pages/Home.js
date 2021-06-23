913d070b7bbd0358e878b03913e7bde965403f7d
Italo Aurelio <italoaurelior@gmail.com>
2021-06-22 21:10:21 -0300

    Add example hook useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Text,
   View,
@@ -13,14 +13,28 @@ import {SkillCard} from '../components/SkillCard';
 export const Home = () => {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
 
   function handleAddSkill() {
     setMySkills(oldState => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good night');
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello iaurg!</Text>
+      <Text style={styles.greetings}>{greeting}</Text>
       <TextInput
         style={styles.input}
         placeholder="New Skill"
@@ -31,7 +45,6 @@ export const Home = () => {
       <Button onPress={handleAddSkill} />
 
       <Text style={[styles.title, {marginVertical: 45}]}>My Skills</Text>
-
       <FlatList
         data={mySkills}
         keyExtractor={item => item}
@@ -60,5 +73,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 12,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: '#fff',
   },
 });
